13147998022304fb5e478ad9db112d80a729ab9a
Ryan Novak <rnovakdevacc@gmail.com>
2023-08-26 21:48:25 -0400

    Implement computer choice function
     - Set the background color of the page to black.
     - Implemented getComputerChoice function to randomly choose and return
    rock, paper, or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+function getComputerChoice() {
+    let choiceNum = Math.floor(Math.random() * 3);
+    if (choiceNum === 1) {
+        return 'rock';
+    } else if (choiceNum === 2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
