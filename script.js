25004dcc0c48793d45d30618c0e22f5535e084c3
Ryan Novak <rnovakdevacc@gmail.com>
2023-08-26 22:17:31 -0400

    Implement single round game function
     - Compares user and computer input and logs a win or loss message

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+const playerSelection = 'Rock'
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+
 function getComputerChoice() {
     let choiceNum = Math.floor(Math.random() * 3);
     if (choiceNum === 1) {
-        return 'rock';
+        return 'ROCK';
     } else if (choiceNum === 2) {
-        return 'paper';
+        return 'PAPER';
     } else {
-        return 'scissors';
+        return 'SCISSORS';
     }
 }
 
-let computerChoice = getComputerChoice();
-console.log(computerChoice);
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toUpperCase();
+    let computer = computerSelection;
+    if (computer === player) {
+        return 'Tie!';
+    } else if (computer === "ROCK" && player === "PAPER") {
+        return "You Win! Paper beats Rock";
+    } else if (computer === "ROCK" && player === "SCISSORS") {
+        return "You Lose! Rock beats Scissors";
+    } else if (computer === "PAPER" && player === "SCISSORS") {
+        return "You Win! Scissors beats Paper";
+    } else if (computer === "PAPER" && player === "ROCK") {
+        return "You Lose! Paper beats Rock";
+    } else if (computer === "SCISSORS" && player === "PAPER") {
+        return "You Lose! Scissors beats Paper";
+    } else if (computer === "SCISSORS" && player === "ROCK") {
+        return "You Win! Rock beats Scissors";
+    } else {
+        return 'Unexpected input';
+    }
+}
